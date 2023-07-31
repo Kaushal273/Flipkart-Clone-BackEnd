f4eda29c1d8d6550bc21f59263705b947d889f5c
Kaushal273 <kaushalyadav536@gmail.com>
2023-07-31 23:02:53 +0530

    login/signup dialog added

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ import Connection from './database/db.js';
 import express from 'express';
 import dotenv from 'dotenv';
 import DefaultData from './default.js';
+import Router from './routes/route.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+
 
 const app = express();
 
 dotenv.config();
+
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cors());
+app.use('/',Router);
 
 const Port = 8000;
 
